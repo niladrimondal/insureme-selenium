b2e7419de1da0e1f548712a301a4a230d6a1b6aa
NiladriSMondal <niladrisankarmondal.mondal@gmail.com>
2023-03-18 12:58:04 +0530

    updated for linux

--- src/main/java/insuremeseleniumproject/insureMe.java
+++ src/main/java/insuremeseleniumproject/insureMe.java
@@ -18,7 +18,14 @@ public class insureMe {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","/home/ubuntu/chromedriver");
+		
 		ChromeOptions chromeOptions = new ChromeOptions();
+		//chromeOptions.addArguments("--headless");
+		//chromeOptions.addArguments("--no-sandbox");
+		//chromeOptions.addArguments("--disable-dev-shm-usage");
+		
+		
 		WebDriver driver = new ChromeDriver(chromeOptions);
 		driver.get("http://localhost:8081/contact.html");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -36,11 +43,11 @@ public class insureMe {
 			System.out.println("Script Failed");
 		}
 		
-		
-		
+		//take creenshot of the result
 		TakesScreenshot scrShot = ((TakesScreenshot)driver);
 		File screenShot = scrShot.getScreenshotAs(OutputType.FILE);
 		File destFile = new File ("C:\\chromedriver\\test-reports\\scr.png");
+		//File destFile = new File ("//home/ubuntu//scr.png");
 		FileUtils.copyFile(screenShot, destFile);
 		
 		Thread.sleep(3000);

edda483a8cf02e2d88926f9a558c18e8bc6ca0b2
NiladriSMondal <niladrisankarmondal.mondal@gmail.com>
2023-07-22 02:00:28 +0530

    updated

--- src/main/java/insuremeseleniumproject/insureMe.java
+++ src/main/java/insuremeseleniumproject/insureMe.java
@@ -18,8 +18,8 @@ public class insureMe {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 		//System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
-		//System.setProperty("webdriver.chrome.driver","//home//ubuntu//chromedriver");
-		System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/chromedriver");
+		System.setProperty("webdriver.chrome.driver","//home//ubuntu//chromedriver-linux64//chromedriver");
+		//System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/chromedriver");
 		
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("--headless");
@@ -29,12 +29,12 @@ public class insureMe {
 		
 		WebDriver driver = new ChromeDriver(chromeOptions);
 		//driver.get("http://localhost:8081/contact.html");
-		driver.get("http://44.198.176.126:8084/contact.html");
+		driver.get("http://54.176.199.149:8084/contact.html");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.findElement(By.id("inputName")).sendKeys("Niladri Sankar Mondal");
 		driver.findElement(By.id("inputNumber")).sendKeys("8431630203");
 		driver.findElement(By.id("inputMail")).sendKeys("niladrimondal.mondal@gmail.com");
-		driver.findElement(By.id("inputMessage")).sendKeys("Hello Insurer");
+		driver.findElement(By.id("inputMessage")).sendKeys("Hello Insurer Niladri");
 		driver.findElement(By.id("my-button")).click();
 		
 		String message = driver.findElement(By.id("response")).getText();
@@ -49,9 +49,9 @@ public class insureMe {
 		TakesScreenshot scrShot = ((TakesScreenshot)driver);
 		File screenShot = scrShot.getScreenshotAs(OutputType.FILE);
 		//File destFile = new File ("C:\\chromedriver\\test-reports\\scr.png");
-		//File destFile = new File ("//home/ubuntu//scr.png");
+		File destFile = new File ("//home/ubuntu//scr.png");
 		//File destFile = new File ("//var/lib/jenkins//scr.png");
-		File destFile = new File ("./scr.png");
+		//File destFile = new File ("./scr.png");
 		FileUtils.copyFile(screenShot, destFile);
 		
 		Thread.sleep(3000);
